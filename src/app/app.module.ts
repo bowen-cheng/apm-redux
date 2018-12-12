0706a39db39c993a7fb0647aa2de4d145e6ed635
Bowen Cheng <blueicycky@gmail.com>
2018-12-12 07:46:00 -0500

    Configure Redux dev tools to allow debugging with the browser extension

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 // Imports for loading & configuring the in-memory web api
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { ProductData } from './products/product-data';
 
+import { environment } from '../environments/environment.prod';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -26,7 +28,13 @@ import { UserModule } from './user/user.module';
     UserModule,
     AppRoutingModule,
     // $$: Initialize the store with an empty object as the reducer placeholder
-    StoreModule.forRoot({})
+    StoreModule.forRoot({}),
+    // $$: configure the redux dev tools
+    StoreDevtoolsModule.instrument({
+      name: 'Demo app',
+      maxAge: 25,
+      logOnly: environment.production
+    })
   ],
   declarations: [
     AppComponent,
