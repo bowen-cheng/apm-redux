3a921bc011db729582af9050f3665e16153e451e
Bowen Cheng <blueicycky@gmail.com>
2018-12-16 20:19:00 -0500

    Define selectors for Product state

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,19 +1,21 @@
+import * as globalStates from '../../state/app.state';
 import { Product } from '../product';
-
-import * as globalStates from '../../state/app.state'
 
 //$$: The following sate should be imported instead of the global one
 export interface AppState extends globalStates.AppState {
   products: ProductState;
 }
 
+/**
+ * State type definition of Product feature module
+ */
 export interface ProductState {
   products: Product[];
   currentProduct: Product;
   showProductCode: boolean;
 }
 
-const initialState: ProductState =  {
+const initialState: ProductState = {
   products: [],
   currentProduct: null,
   showProductCode: true
