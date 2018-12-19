f280082f25197dec1e691ad6d7da209d94d9a88c
Bowen Cheng <blueicycky@gmail.com>
2018-12-19 00:06:23 -0500

    Implement action types for Product module

---
+++ src/app/products/state/product.action.ts
@@ -0,0 +1,37 @@
+import { Action } from '@ngrx/store';
+import { Product } from '../product';
+
+// $$: Define actions as named constants
+export enum ProductActionType {
+  ToggleProductCode = '[Product] Toggle product code',
+  SetCurrentProduct = '[Product] Set current product',
+  ClearCurrentProduct = '[Product] Clear current product',
+  InitializeCurrentProduct = '[Product] I nitialize current product'
+}
+
+// $$: build action creators
+export class ToggleProductCode implements Action {
+  readonly type: ProductActionType.ToggleProductCode;
+
+  constructor(public  payload: boolean) {}
+}
+
+export class SetCurrentProduct implements Action {
+  readonly type: ProductActionType.SetCurrentProduct;
+
+  constructor(public  payload: Product) {}
+}
+
+export class ClearCurrentProduct implements Action {
+  readonly type: ProductActionType.ClearCurrentProduct;
+}
+
+export class InitializeCurrentProduct implements Action {
+  readonly type: ProductActionType.InitializeCurrentProduct;
+}
+
+// $$: Define a union type for action creators
+export type ProductAction = ToggleProductCode
+| SetCurrentProduct
+| ClearCurrentProduct
+| InitializeCurrentProduct
